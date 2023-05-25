2ba2e1f4671467f62bea491b9b21ad2e0befcb53
Tevon Davis <tevondavisjc@gmail.com>
2023-05-24 23:50:49 -0500

    record-date default to current timestamp

--- database/migrations/2023_05_25_010214_create_coupons_table.php
+++ database/migrations/2023_05_25_010214_create_coupons_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->bigIncrements('coupon_id');
             $table->text('isin_code')->nullable();
             $table->dateTime('payment_date')->nullable();
-            $table->dateTime('record_date')->nullable();
+            $table->dateTime('record_date')->default(DB::raw('CURRENT_TIMESTAMP'))->nullable();
             $table->integer('status_id')->nullable();
             $table->foreign('status_id')->references('status_id')->on('status_codes');
             $table->foreign('isin_code')->references('isin_code')->on('securities');
